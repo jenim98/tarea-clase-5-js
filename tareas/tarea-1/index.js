f7ea657322373ae983730a7713139b5a5c182225
jenim98 <jenifer.michillon@gmail.com>
2022-09-02 19:43:52 -0300

    corrección de errores
    Por prolijidad eliminé el enunciado del ejercicio moviéndolo a un nuevo archivo .txt, además eliminé el .value de la variable "$calcularSalarioMensual" ya que no permitía que el botón funcionara correctamente

--- tareas/tarea-1/index.js
+++ tareas/tarea-1/index.js
@@ -1,12 +1,8 @@
-//TAREA: crear un formulario donde un usuario pueda ingresar su salario anual.
-//cuando el usuario haga click en el botón "calcular", mostrar el salario mensual
-// en una caja de texto deshabilitada. --> <input type="text" disabled id="salario-mensual"/>
-
 function calcularSalarioMensual(salarioAnual){
     return salarioAnual / 12;
 }
   
-const $calcularSalarioMensual = document.querySelector('#calcular-salario-mensual').value;
+const $calcularSalarioMensual = document.querySelector('#calcular-salario-mensual');
 
 $calcularSalarioMensual.onclick = function(){
     const salarioAnual = Number(document.querySelector('#salario-anual').value);
