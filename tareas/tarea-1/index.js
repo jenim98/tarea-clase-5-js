5972427a55b5a625ced13c2268306f35bd3d8f3f
jenim98 <jenifer.michillon@gmail.com>
2022-09-02 19:00:40 -0300

    crea el archivo js
    Este archivo fue creado para realizar la codificación necesaria para resolver el ejercicio

---
+++ tareas/tarea-1/index.js
@@ -0,0 +1,17 @@
+//TAREA: crear un formulario donde un usuario pueda ingresar su salario anual.
+//cuando el usuario haga click en el botón "calcular", mostrar el salario mensual
+// en una caja de texto deshabilitada. --> <input type="text" disabled id="salario-mensual"/>
+
+function calcularSalarioMensual(salarioAnual){
+    return salarioAnual / 12;
+}
+  
+const $calcularSalarioMensual = document.querySelector('#calcular-salario-mensual').value;
+
+$calcularSalarioMensual.onclick = function(){
+    const salarioAnual = Number(document.querySelector('#salario-anual').value);
+    const salarioMensual = calcularSalarioMensual(salarioAnual);
+    document.querySelector('#salario-mensual').value = salarioMensual;
+  
+    return false;
+}
