4aeabd2862b9042d1d048a16bbc770c0051165c4
jenim98 <jenifer.michillon@gmail.com>
2022-09-08 21:57:08 -0300

    corrección y finalización
    Puse parámetros en las funciones y finalicé el ejercicio

--- tareas/tarea-4/index.js
+++ tareas/tarea-4/index.js
@@ -7,7 +7,7 @@ for(i = 0; i < $numerosLista.length; i++){
     numerosArray.push(Number($numerosLista[i].innerText))
 }
 
-function calcularPromedio() {
+function calcularPromedio(numerosArray) {
     let sumaTotal = 0;
     for(i = 0; i < numerosArray.length; i++){
         sumaTotal += numerosArray[i];
@@ -15,7 +15,7 @@ function calcularPromedio() {
     return sumaTotal / numerosArray.length;
 }
 
-function mostrarElNumeroMasPequenio() {
+function mostrarElNumeroMasPequenio(numerosArray) {
     let numeroPequenio = numerosArray[0];
     for(i = 1; i < numerosArray.length; i++){
         if(numerosArray[i]<numeroPequenio){
@@ -25,14 +25,30 @@ function mostrarElNumeroMasPequenio() {
     }
 }
 
-function mostrarElNumeroMasGrande() {
+function mostrarElNumeroMasGrande(numerosArray) {
     let numeroMayor = numerosArray[0];
     for(i = 1; i < numerosArray.length; i++){
         if(numerosArray[i]>numeroMayor){
             numeroMayor = numerosArray[i];
         }
-        return numeroMayor;
+        
+    }
+    return numeroMayor;
+}
+
+function mostrarNumeroRepetido(numerosArray) {
+    let numeroRepetido = numerosArray[0];
+    for(i = 0; i = numerosArray.length; i++){
+        if(numerosArray[i]=numeroRepetido){
+            numeroRepetido = numerosArray[i];
+        }
+        return numeroRepetido;
     }
 }
-console.log(mostrarElNumeroMasGrande());
 
+$numerosResultado.onclick = function() {
+    document.querySelector('#promedio').innerText = "El promedio es " + calcularPromedio(numerosArray);
+    document.querySelector('#numero-pequeño').innerText = "El número más pequeño es " + mostrarElNumeroMasPequenio(numerosArray);
+    document.querySelector('#numero-grande').innerText = "El número más grande es " + mostrarElNumeroMasGrande(numerosArray);
+    document.querySelector('#numero-frecuente').innerText = "El número más frecuente es " + mostrarNumeroRepetido(numerosArray);
+}
